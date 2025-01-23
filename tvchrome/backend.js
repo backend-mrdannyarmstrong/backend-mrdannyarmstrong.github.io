3442508312fd78e59db775f2e29141404fece7ba
Daniel <98234578+mrdannyarmstrong@users.noreply.github.com>
2025-01-23 16:00:11 -0500

    Add files via upload

--- tvchrome/backend.js
+++ tvchrome/backend.js
@@ -1,6 +1,6 @@
 function getchannels() {
     channels = [
-        ["LIST", "START"],
+        ["https://devstreaming-cdn.apple.com/videos/streaming/examples/bipbop_4x3/bipbop_4x3_variant.m3u8", "nochan.gif"],
         ["NULL", "NULL"],
         ["https://service-stitcher.clusters.pluto.tv/v1/stitch/embed/hls/channel/5f36d726234ce10007784f2a/master.m3u8?advertisingId=channel&appName=rokuchannel&appVersion=1.0&bmodel=bm1&channel_id=channel&content=channel&content_rating=ROKU_ADS_CONTENT_RATING&content_type=livefeed&coppa=false&deviceDNT=1&deviceId=channel&deviceMake=rokuChannel&deviceModel=web&deviceType=rokuChannel&deviceVersion=1.0&embedPartner=rokuChannel&genre=ROKU_ADS_CONTENT_GENRE&is_lat=1&platform=web&rdid=channel&studio_id=viacom&tags=ROKU_CONTENT_TAGS", "https://backend-mrdannyarmstrong.github.io/tvchrome/ch2.gif"],
         ["https://v1.weatherscan.net/", "https://backend-mrdannyarmstrong.github.io/tvchrome/ch3.gif"],
