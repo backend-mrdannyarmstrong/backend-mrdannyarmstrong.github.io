4a1400290b7b0e225bb285efcba81f87e22952e8
Daniel <98234578+mrdannyarmstrong@users.noreply.github.com>
2025-02-13 12:00:37 -0500

    Add files via upload

---
+++ tvharmony/back.js
@@ -0,0 +1,22 @@
+function getchannels() {
+    channels = [
+        ["https://devstreaming-cdn.apple.com/videos/streaming/examples/bipbop_4x3/bipbop_4x3_variant.m3u8", "nochan.gif"],
+        ["NULL", "NULL"],
+        ["NULL", "NULL"],
+        ["NULL", "NULL"],
+        ["NULL", "NULL"],
+        ["NULL", "NULL"],
+        ["NULL", "NULL"],
+        ["NULL", "NULL"],
+        ["NULL", "NULL"],
+        ["NULL", "NULL"],
+        ["NULL", "NULL"],
+        ["NULL", "NULL"],
+        ["NULL", "NULL"],
+        ["NULL", "NULL"],
+        ["NULL", "NULL"],
+        ["NULL", "NULL"],
+        ["NULL", "NULL"],
+    ];
+    return channels;
+}
