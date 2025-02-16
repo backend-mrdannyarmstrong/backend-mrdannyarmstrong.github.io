496e20bdd98a39ddc6e4248e19237aa15571d3d3
Daniel <98234578+mrdannyarmstrong@users.noreply.github.com>
2025-02-16 14:34:44 -0500

    Add files via upload

--- tvharmony/back.js
+++ tvharmony/back.js
@@ -9,13 +9,13 @@ function getchannels() {
         ["http://fl5.moveonjoy.com/AMC_NETWORK/index.m3u8", "https://backend-mrdannyarmstrong.github.io/tvharmony/ch6.gif"],
         ["https://a-cdn.herringnetwork.com/affiliate/awee/playlist.m3u8", "https://backend-mrdannyarmstrong.github.io/tvharmony/ch7.gif"],
         ["https://pluto.tv/us/live-tv/5ca673e0d0bd6c2689c94ce3", "https://backend-mrdannyarmstrong.github.io/tvharmony/ch8.gif"],
-        ["NULL", "https://backend-mrdannyarmstrong.github.io/tvharmony/ch9.gif"],
-        ["NULL", "https://backend-mrdannyarmstrong.github.io/tvharmony/ch10.gif"],
+        ["https://d1zx6l1dn8vaj5.cloudfront.net/out/v1/b89cc37caa6d418eb423cf092a2ef970/index.m3u8", "https://backend-mrdannyarmstrong.github.io/tvharmony/ch9.gif"],
+        ["https://d277k9d1h9dro4.cloudfront.net/out/v1/293e7c3464824cbd8818ab8e49dc5fe9/index.m3u8", "https://backend-mrdannyarmstrong.github.io/tvharmony/ch10.gif"],
         ["NULL", "https://backend-mrdannyarmstrong.github.io/tvharmony/ch11.gif"],
         ["NULL", "https://backend-mrdannyarmstrong.github.io/tvharmony/ch12.gif"],
         ["NULL", "https://backend-mrdannyarmstrong.github.io/tvharmony/ch13.gif"],
-        ["NULL", "https://backend-mrdannyarmstrong.github.io/tvharmony/ch14.gif"],
-        ["NULL", "https://backend-mrdannyarmstrong.github.io/tvharmony/ch15.gif"],
+        ["https://cdn.jmvstream.com/w/LVW-10842/LVW10842_513N26MDBL/chunklist.m3u8", "https://backend-mrdannyarmstrong.github.io/tvharmony/ch14.gif"],
+        ["https://cdn.jmvstream.com/w/LVW-10801/LVW10801_Xvg4R0u57n/playlist.m3u8", "https://backend-mrdannyarmstrong.github.io/tvharmony/ch15.gif"],
     ];
     return channels;
 }
