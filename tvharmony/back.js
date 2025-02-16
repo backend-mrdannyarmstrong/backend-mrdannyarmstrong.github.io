2ed9d81e71fc8b19d45c2ae393a722f31e0ceb31
Daniel <98234578+mrdannyarmstrong@users.noreply.github.com>
2025-02-15 20:08:11 -0500

    Add files via upload

---
+++ tvharmony/back.js
@@ -0,0 +1,22 @@
+function getchannels() {
+    channels = [
+        ["https://devstreaming-cdn.apple.com/videos/streaming/examples/bipbop_4x3/bipbop_4x3_variant.m3u8", "nochan.gif"],
+        ["NULL", "NULL"],
+        ["NULL", "NULL"],
+        ["http://fl2.moveonjoy.com/NEWS_NATION/index.m3u8", "backend-mrdannyarmstrong.github.io/tvharmony/ch3.gif"],
+        ["https://v2.weatherscan.net/", "backend-mrdannyarmstrong.github.io/tvharmony/ch4.gif"],
+        ["NULL", "NULL"],
+        ["NULL", "NULL"],
+        ["NULL", "NULL"],
+        ["NULL", "NULL"],
+        ["NULL", "NULL"],
+        ["NULL", "NULL"],
+        ["NULL", "NULL"],
+        ["NULL", "NULL"],
+        ["NULL", "NULL"],
+        ["NULL", "NULL"],
+        ["NULL", "NULL"],
+        ["NULL", "NULL"],
+    ];
+    return channels;
+}
