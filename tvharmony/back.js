d18269cdf6f83907e8488655ea48c9a8a02f1165
Daniel <98234578+mrdannyarmstrong@users.noreply.github.com>
2025-02-15 22:18:43 -0500

    Add files via upload

--- tvharmony/back.js
+++ tvharmony/back.js
@@ -2,7 +2,7 @@ function getchannels() {
     channels = [
         ["https://devstreaming-cdn.apple.com/videos/streaming/examples/bipbop_4x3/bipbop_4x3_variant.m3u8", "nochan.gif"],
         ["NULL", "NULL"],
-        ["http://cfd-v4-service-channel-stitcher-use1-1.prd.pluto.tv/stitch/hls/channel/640a64bd73e013000893d4e0/master.m3u8?appName=web&appVersion=unknown&clientTime=0&deviceDNT=0&deviceId=6c2a9f23-30d3-11ef-9cf5-e9ddff8ff496&deviceMake=Chrome&deviceModel=web&deviceType=web&deviceVersion=unknown&includeExtendedEvents=false&serverSideAds=false&sid=c1a2f62c-0e00-4ba7-b555-34d14d59fee4", "https://backend-mrdannyarmstrong.github.io/tvharmony/ch2.gif"],
+        ["https://apollo.production-public.tubi.io/live/ac-bbc-earth2.m3u8", "https://backend-mrdannyarmstrong.github.io/tvharmony/ch2.gif"],
         ["http://fl2.moveonjoy.com/NEWS_NATION/index.m3u8", "https://backend-mrdannyarmstrong.github.io/tvharmony/ch3.gif"],
         ["https://v2.weatherscan.net/", "https://backend-mrdannyarmstrong.github.io/tvharmony/ch4.gif"],
         ["https://www.youtube.com/embed/HuOHu5XSTSk", "https://backend-mrdannyarmstrong.github.io/tvharmony/ch5.gif"],
