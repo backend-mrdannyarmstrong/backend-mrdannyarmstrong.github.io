46a73265bf0af120fd9650567fb5c7dcbe27a580
Daniel <98234578+mrdannyarmstrong@users.noreply.github.com>
2025-02-16 11:39:30 -0500

    Add files via upload

--- tvharmony/back.js
+++ tvharmony/back.js
@@ -8,7 +8,7 @@ function getchannels() {
         ["https://www.youtube.com/embed/HuOHu5XSTSk", "https://backend-mrdannyarmstrong.github.io/tvharmony/ch5.gif"],
         ["http://fl5.moveonjoy.com/AMC_NETWORK/index.m3u8", "https://backend-mrdannyarmstrong.github.io/tvharmony/ch6.gif"],
         ["https://a-cdn.herringnetwork.com/affiliate/awee/playlist.m3u8", "https://backend-mrdannyarmstrong.github.io/tvharmony/ch7.gif"],
-        ["http://cfd-v4-service-channel-stitcher-use1-1.prd.pluto.tv/stitch/hls/channel/5ca673e0d0bd6c2689c94ce3/master.m3u8?appName=web&appVersion=unknown&clientTime=0&deviceDNT=0&deviceId=6c2a5108-30d3-11ef-9cf5-e9ddff8ff496&deviceMake=Chrome&deviceModel=web&deviceType=web&deviceVersion=unknown&includeExtendedEvents=false&serverSideAds=false&sid=5730720c-9e54-41d9-b73d-eab8fdac7291", "https://backend-mrdannyarmstrong.github.io/tvharmony/ch8.gif"],
+        ["https://pluto.tv/us/live-tv/5ca673e0d0bd6c2689c94ce3", "https://backend-mrdannyarmstrong.github.io/tvharmony/ch8.gif"],
         ["NULL", "https://backend-mrdannyarmstrong.github.io/tvharmony/ch9.gif"],
         ["NULL", "https://backend-mrdannyarmstrong.github.io/tvharmony/ch10.gif"],
         ["NULL", "https://backend-mrdannyarmstrong.github.io/tvharmony/ch11.gif"],
