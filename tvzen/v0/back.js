4556e001a5195b97987009e34e56bad92e1701e2
Daniel <98234578+mrdannyarmstrong@users.noreply.github.com>
2025-03-05 14:53:15 -0500

    Add files via upload

--- tvzen/v0/back.js
+++ tvzen/v0/back.js
@@ -1,1 +1,26 @@
-//back
+function getchannels() {
+    channels = [
+        ["https://devstreaming-cdn.apple.com/videos/streaming/examples/bipbop_4x3/bipbop_4x3_variant.m3u8", "nochan.gif"],
+        ["NULL", "nochan.gif"],
+        ["NULL", "nochan.gif"],
+        ["NULL", "nochan.gif"],
+        ["NULL", "nochan.gif"],
+        ["NULL", "nochan.gif"],
+        ["NULL", "nochan.gif"],
+        ["NULL", "nochan.gif"],
+        ["NULL", "nochan.gif"],
+        ["NULL", "nochan.gif"],
+        ["NULL", "nochan.gif"],
+        ["NULL", "nochan.gif"],
+        ["NULL", "nochan.gif"],
+        ["NULL", "nochan.gif"],
+        ["NULL", "nochan.gif"],
+        ["NULL", "nochan.gif"],
+        ["NULL", "nochan.gif"],
+        ["NULL", "nochan.gif"],
+        ["NULL", "nochan.gif"],
+        ["NULL", "nochan.gif"],
+        ["NULL", "nochan.gif"],
+    ];
+    return channels;
+}
