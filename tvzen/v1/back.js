b9cb82cae6e48c1ff3222060fbe00a55d882eed9
Daniel <98234578+mrdannyarmstrong@users.noreply.github.com>
2025-03-05 21:32:53 -0500

    Add files via upload

--- tvzen/v1/back.js
+++ tvzen/v1/back.js
@@ -3,7 +3,7 @@ function getchannels() {
         ["https://devstreaming-cdn.apple.com/videos/streaming/examples/bipbop_4x3/bipbop_4x3_variant.m3u8", "nochan.gif"],
         ["NULL", "nochan.gif"],
         ["https://apollo.production-public.tubi.io/live/ac-bbc-earth2.m3u8", "https://backend-mrdannyarmstrong.github.io/tvzen/v1/ch2.png"],
-        ["http://fl2.moveonjoy.com/NEWS_NATION/index.m3u8", "https://backend-mrdannyarmstrong.github.io/tvzen/v1/ch3.png]"],
+        ["http://fl2.moveonjoy.com/NEWS_NATION/index.m3u8", "https://backend-mrdannyarmstrong.github.io/tvzen/v1/ch3.png"],
         ["https://v2.weatherscan.net/", "https://backend-mrdannyarmstrong.github.io/tvzen/v1/ch4.png"],
         ["http://cfd-v4-service-channel-stitcher-use1-1.prd.pluto.tv/stitch/hls/channel/5812b7d3249444e05d09cc49/master.m3u8?appName=web&appVersion=unknown&clientTime=0&deviceDNT=0&deviceId=6c26a781-30d3-11ef-9cf5-e9ddff8ff496&deviceMake=Chrome&deviceModel=web&deviceType=web&deviceVersion=unknown&includeExtendedEvents=false&serverSideAds=false&sid=6903cfae-183e-4d3b-a482-b7acf38ce356", "https://backend-mrdannyarmstrong.github.io/tvzen/v1/ch5.png"],
         ["https://www.youtube.com/embed/HuOHu5XSTSk", "https://backend-mrdannyarmstrong.github.io/tvzen/v1/ch6.png"],
