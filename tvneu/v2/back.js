752608e937abf8e9a25997cb6f53710e046d758c
Daniel <98234578+mrdannyarmstrong@users.noreply.github.com>
2025-03-10 22:46:20 -0400

    Add files via upload

--- tvneu/v2/back.js
+++ tvneu/v2/back.js
@@ -18,6 +18,7 @@ function getchannels() {
         ["https://service-stitcher.clusters.pluto.tv/v3/stitch/embed/hls/channel/5f36d726234ce10007784f2a/master.m3u8?advertisingId=channel&appName=rokuchannel&appVersion=1.0&bmodel=bm1&channel_id=channel&content=channel&content_rating=ROKU_ADS_CONTENT_RATING&content_type=livefeed&coppa=false&deviceDNT=1&deviceId=channel&deviceMake=rokuChannel&deviceModel=web&deviceType=rokuChannel&deviceVersion=1.0&embedPartner=rokuChannel&genre=ROKU_ADS_CONTENT_GENRE&is_lat=1&platform=web&rdid=channel&studio_id=viacom&tags=ROKU_CONTENT_TAGS", "https://backend-mrdannyarmstrong.github.io/tvneu/v2/banners/bobross.svg"],
         ["NULL", "NULL"],
         ["NULL", "NULL"],
+        ["NULL", "NULL"],
         ["NULL", "NULL"]
     ];
     return channeldb;
