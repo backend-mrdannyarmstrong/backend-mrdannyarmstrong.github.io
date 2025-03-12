c162ce0c0804da57c4fe1d41a8d8bda720d35880
Daniel <98234578+mrdannyarmstrong@users.noreply.github.com>
2025-03-12 15:27:04 -0400

    Add files via upload

--- tvharmony/v1/back.js
+++ tvharmony/v1/back.js
@@ -1,1 +1,21 @@
-//Add code here
+function getchannels() {
+    channels = [
+        ["https://devstreaming-cdn.apple.com/videos/streaming/examples/bipbop_4x3/bipbop_4x3_variant.m3u8", "nochan.gif"],
+        ["NULL", "NULL"],
+        ["https://apollo.production-public.tubi.io/live/ac-bbc-earth2.m3u8", "https://backend-mrdannyarmstrong.github.io/tvharmony/v1/ch2.gif"],
+        ["https://content-ause1.uplynk.com/channel/4bb4901b934c4e029fd4c1abfc766c37/e.m3u8?v=2&ad=live&expand=ad_live&repl=aboi&ad.url=https%3A%2F%2Fwww.scrippsnews.com%2F&ad.cust_params=%26inv%3Dss%3A%20on-domain%3A%20ew%20scripps%3A%20scripps%20news%3A%20desktop%3A%20unknown%3A%20web%3A%20live%26user_id%3Dundefined%26pxconfig%3Dweb-live&pbs=13477f40297541f89bf2b20f98541096", "https://backend-mrdannyarmstrong.github.io/tvharmony/v1/ch3.gif"],
+        ["https://v2.weatherscan.net/", "https://backend-mrdannyarmstrong.github.io/tvharmony/v1/ch4.gif"],
+        ["https://www.youtube.com/embed/HuOHu5XSTSk", "https://backend-mrdannyarmstrong.github.io/tvharmony/v1/ch5.gif"],
+        ["https://tvpass.org/live/EEast/sd", "https://backend-mrdannyarmstrong.github.io/tvharmony/v1/ch6.gif"],
+        ["https://a-cdn.herringnetwork.com/affiliate/awee/playlist.m3u8", "https://backend-mrdannyarmstrong.github.io/tvharmony/v1/ch7.gif"],
+        ["http://cfd-v4-service-channel-stitcher-use1-1.prd.pluto.tv/stitch/hls/channel/5ca673e0d0bd6c2689c94ce3/master.m3u8?appName=web&appVersion=unknown&clientTime=0&deviceDNT=0&deviceId=6c2a5108-30d3-11ef-9cf5-e9ddff8ff496&deviceMake=Chrome&deviceModel=web&deviceType=web&deviceVersion=unknown&includeExtendedEvents=false&serverSideAds=false&sid=5730720c-9e54-41d9-b73d-eab8fdac7291", "https://backend-mrdannyarmstrong.github.io/tvharmony/v1/ch8.gif"],
+        ["https://d1zx6l1dn8vaj5.cloudfront.net/out/v1/b89cc37caa6d418eb423cf092a2ef970/index.m3u8", "https://backend-mrdannyarmstrong.github.io/tvharmony/v1/ch9.gif"],
+        ["https://d277k9d1h9dro4.cloudfront.net/out/v1/293e7c3464824cbd8818ab8e49dc5fe9/index.m3u8", "https://backend-mrdannyarmstrong.github.io/tvharmony/v1/ch10.gif"],
+        ["https://video-auth2.iol.pt/live_tvi_ficcao/live_tvi_ficcao/edge_servers/tvificcao-720p/playlist.m3u8", "https://backend-mrdannyarmstrong.github.io/tvharmony/v1/ch11.gif"],
+        ["https://cdn.jmvstream.com/w/LVW-10842/LVW10842_513N26MDBL/v1/chunklist.m3u8", "https://backend-mrdannyarmstrong.github.io/tvharmony/v1/ch12.gif"],
+        ["https://cdn.jmvstream.com/w/LVW-10801/LVW10801_Xvg4R0u57n/playlist.m3u8", "https://backend-mrdannyarmstrong.github.io/tvharmony/v1/ch13.gif"],
+        ["https://service-stitcher.clusters.pluto.tv/v1/stitch/embed/hls/channel/5f12136385bccc00070142ed/master.m3u8?advertisingId=channel&appName=rokuchannel&appVersion=1.0&bmodel=bm1&channel_id=channel&content=channel&content_rating=ROKU_ADS_CONTENT_RATING&content_type=livefeed&coppa=false&deviceDNT=1&deviceId=channel&deviceMake=rokuChannel&deviceModel=web&deviceType=rokuChannel&deviceVersion=1.0&embedPartner=rokuChannel&genre=ROKU_ADS_CONTENT_GENRE&is_lat=1&platform=web&rdid=channel&studio_id=viacom&tags=ROKU_CONTENT_TAGS", "https://backend-mrdannyarmstrong.github.io/tvharmony/v1/ch14.gif"],
+        ["https://service-stitcher.clusters.pluto.tv/v1/stitch/embed/hls/channel/5f1213ba0ecebc00070e170f/master.m3u8?advertisingId=channel&appName=rokuchannel&appVersion=1.0&bmodel=bm1&channel_id=channel&content=channel&content_rating=ROKU_ADS_CONTENT_RATING&content_type=livefeed&coppa=false&deviceDNT=1&deviceId=channel&deviceMake=rokuChannel&deviceModel=web&deviceType=rokuChannel&deviceVersion=1.0&embedPartner=rokuChannel&genre=ROKU_ADS_CONTENT_GENRE&is_lat=1&platform=web&rdid=channel&studio_id=viacom&tags=ROKU_CONTENT_TAGS", "https://backend-mrdannyarmstrong.github.io/tvharmony/v1/ch15.gif"],
+    ];
+    return channels;
+}
