69fe0308f68c37ec682ae771b4b65b76bdf5797f
Daniel <98234578+mrdannyarmstrong@users.noreply.github.com>
2025-03-27 11:17:33 -0400

    Add files via upload

---
+++ tvneu/v4/intl.js
@@ -0,0 +1,21 @@
+function checkupdate() {
+    needupdate = 0;
+    return needupdate;
+}
+function geturl() {
+    url = ""
+    return url
+}
+function getchannels() {
+    let channeldb = [
+        ["../index.html", "https://backend-mrdannyarmstrong.github.io/tvneu/v4/banners/legacy/modern.svg"],
+        ["NULL", "NULL"],
+        ["NULL", "NULL"],
+        ["NULL", "NULL"],
+        ["NULL", "NULL"],
+        ["NULL", "NULL"],
+        ["NULL", "NULL"],
+        ["NULL", "NULL"]
+    ];
+    return channeldb;
+}
