9a2900123db1f1a7ab8f827035c5f21a48fffa68
Daniel <98234578+mrdannyarmstrong@users.noreply.github.com>
2025-03-26 20:53:01 -0400

    Add files via upload

---
+++ tvneu/v4/legacy.js
@@ -0,0 +1,27 @@
+function checkupdate() {
+    needupdate = 0;
+    return needupdate;
+}
+function geturl() {
+    url = ""
+    return url
+}
+function getchannels() {
+    let channeldb = [
+        ["https://turnerlive.warnermediacdn.com/hls/live/2023168/tnteast/slate/VIDEO_0_3564000.m3u8", "https://backend-mrdannyarmstrong.github.io/tvneu/v4/banners/legacy/tnt.svg"],
+        ["https://a-cdn.herringnetwork.com/affiliate/awee/playlist.m3u8", "https://backend-mrdannyarmstrong.github.io/tvneu/v4/banners/legacy/awe.svg"],
+        ["http://fl2.moveonjoy.com/PARAMOUNT_NETWORK/index.m3u8", "https://backend-mrdannyarmstrong.github.io/tvneu/v4/banners/legacy/paramount.svg"],
+        ["http://fl2.moveonjoy.com/NEWS_NATION/index.m3u8", "https://backend-mrdannyarmstrong.github.io/tvneu/v4/banners/legacy/newsnation.svg"],
+        ["https://turnerlive.warnermediacdn.com/hls/live/2023183/aseast/noslate/VIDEO_1_5128000.m3u8", "https://backend-mrdannyarmstrong.github.io/tvneu/v4/banners/legacy/adultswim.svg"],
+        ["../index.html", "https://backend-mrdannyarmstrong.github.io/tvneu/v4/banners/legacy/modern.svg"],
+        ["NULL", "NULL"],
+        ["NULL", "NULL"],
+        ["NULL", "NULL"],
+        ["NULL", "NULL"],
+        ["NULL", "NULL"],
+        ["NULL", "NULL"],
+        ["NULL", "NULL"],
+        ["NULL", "NULL"]
+    ];
+    return channeldb;
+}
