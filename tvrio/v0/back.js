f361e80ced589ad40c88fa84acc15f734738a271
Daniel <98234578+mrdannyarmstrong@users.noreply.github.com>
2025-04-29 11:56:36 -0400

    Add files via upload

--- tvrio/v0/back.js
+++ tvrio/v0/back.js
@@ -8,7 +8,7 @@ function geturl() {
 }
 function getchannels() {
     let channeldb = [
-        ["http://fl2.moveonjoy.com/FX/index.m3u8", "https://backend-mrdannyarmstrong.github.io/tvrio/v0/banners/fx.svg"],
+        ["https://v1.thetvapp.to/hls/FXEast/index.m3u8", "https://backend-mrdannyarmstrong.github.io/tvrio/v0/banners/fx.svg"],
         ["http://api.toonamiaftermath.com:3000/est/playlist.m3u8", "https://backend-mrdannyarmstrong.github.io/tvrio/v0/banners/toonami.png"],
         ["http://fl2.moveonjoy.com/NEWS_NATION/index.m3u8", "https://backend-mrdannyarmstrong.github.io/tvrio/v0/banners/newsnation.svg"],
         ["https://turnerlive.warnermediacdn.com/hls/live/2023183/aseast/noslate/VIDEO_1_5128000.m3u8", "https://backend-mrdannyarmstrong.github.io/tvrio/v0/banners/adultswim.svg"],
