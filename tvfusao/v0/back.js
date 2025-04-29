f2f25a45b817c1c80c533869d72a05a2e83dbd57
Daniel <98234578+mrdannyarmstrong@users.noreply.github.com>
2025-04-29 12:24:11 -0400

    Add files via upload

--- tvfusao/v0/back.js
+++ tvfusao/v0/back.js
@@ -9,7 +9,7 @@ function geturl() {
 function getchannels() {
     let channeldb = [
         ["https://v1.thetvapp.to/hls/FXEast/index.m3u8", "https://backend-mrdannyarmstrong.github.io/tvfusao/v0/banners/fx.svg"],
-        ["http://api.toonamiaftermath.com:3000/est/playlist.m3u8", "https://backend-mrdannyarmstrong.github.io/tvfusao/v0/banners/toonami.png"],
+        ["http://api.toonamiaftermath.com:3000/est/playlist.m3u8", "https://backend-mrdannyarmstrong.github.io/tvfusao/v0/banners/toonami.svg"],
         ["http://fl2.moveonjoy.com/NEWS_NATION/index.m3u8", "https://backend-mrdannyarmstrong.github.io/tvfusao/v0/banners/newsnation.svg"],
         ["https://turnerlive.warnermediacdn.com/hls/live/2023183/aseast/noslate/VIDEO_1_5128000.m3u8", "https://backend-mrdannyarmstrong.github.io/tvfusao/v0/banners/adultswim.svg"],
     ];
