172c121155c89385a5d9fdfa3ccb90364b98915e
Daniel <98234578+mrdannyarmstrong@users.noreply.github.com>
2025-04-28 22:51:26 -0400

    Add files via upload

---
+++ tvrio/v0/back.js
@@ -0,0 +1,17 @@
+function checkupdate() {
+    needupdate = 0;
+    return needupdate;
+}
+function geturl() {
+    url = ""
+    return url
+}
+function getchannels() {
+    let channeldb = [
+        ["http://fl2.moveonjoy.com/FX/index.m3u8", "https://backend-mrdannyarmstrong.github.io/tvrio/v0/banners/fx.svg"],
+        ["http://api.toonamiaftermath.com:3000/est/playlist.m3u8", "https://backend-mrdannyarmstrong.github.io/tvrio/v0/banners/toonami.png"],
+        ["http://fl2.moveonjoy.com/NEWS_NATION/index.m3u8", "https://backend-mrdannyarmstrong.github.io/tvrio/v0/banners/newsnation.svg"],
+        ["https://turnerlive.warnermediacdn.com/hls/live/2023183/aseast/noslate/VIDEO_1_5128000.m3u8", "https://backend-mrdannyarmstrong.github.io/tvrio/v0/banners/adultswim.svg"],
+    ];
+    return channeldb;
+}
