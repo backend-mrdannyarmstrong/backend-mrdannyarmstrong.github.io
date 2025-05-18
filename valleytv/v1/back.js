3b0b67608ecbb0518df795feefaf8d9e84e5f39a
Daniel <98234578+mrdannyarmstrong@users.noreply.github.com>
2025-05-17 20:43:14 -0400

    Add files via upload

---
+++ valleytv/v1/back.js
@@ -0,0 +1,17 @@
+function checkupdate() {
+    needupdate = 0;
+    return needupdate;
+}
+function geturl() {
+    url = ""
+    return url
+}
+function getchannels() {
+    let channeldb = [
+        ["https://turnerlive.warnermediacdn.com/hls/live/2023168/tnteast/slate/VIDEO_0_3564000.m3u8", "https://backend-megafrog2000.github.io/valleytv/v1/banners/tnt.svg"],
+        ["https://turnerlive.warnermediacdn.com/hls/live/2023183/aseast/noslate/VIDEO_1_5128000.m3u8", "https://backend-megafrog2000.github.io/valleytv/v1/banners/adultswim.svg"],
+        ["https://a-cdn.herringnetwork.com/affiliate/awee/playlist.m3u8", "https://backend-megafrog2000.github.io/valleytv/v1/banners/awe.svg"],
+        ["http://fl2.moveonjoy.com/NEWS_NATION/index.m3u8", "https://backend-megafrog2000.github.io/valleytv/v1/banners/newsnation.svg"]
+    ];
+    return channeldb;
+}
