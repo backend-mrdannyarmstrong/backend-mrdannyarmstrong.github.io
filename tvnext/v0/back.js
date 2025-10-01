006339e07d8deedb54488b6e7cb5cfd3968c3cde
Daniel <98234578+mrdannyarmstrong@users.noreply.github.com>
2025-09-30 22:09:01 -0400

    Add files via upload

--- tvnext/v0/back.js
+++ tvnext/v0/back.js
@@ -1,1 +1,20 @@
-//add code here
+﻿function getchannels() {
+    let channeldb = [
+        ["index.html", "NULL"],
+        ["index2.html#https://livetv-fa.tubi.video/bbc-earth2/playlist.m3u8", "https://backend-mrdannyarmstrong.github.io/tvnext/v0/ch1.png"],
+        ["https://pluto.tv/us/live-tv/5f36d726234ce10007784f2a", "https://backend-mrdannyarmstrong.github.io/tvnext/v0/ch2.png"],
+        ["index2.html#https://fl7.moveonjoy.com/NEWS_NATION/index.m3u8", "https://backend-mrdannyarmstrong.github.io/tvnext/v0/ch3.png"],
+        ["http://v2.weatherscan.net/", "https://backend-mrdannyarmstrong.github.io/tvnext/v0/ch4.png"],
+    ];
+    return channeldb;
+}
+
+function checkupdate() {
+    needupdate = 0;
+    return needupdate;
+}
+
+function geturl() {
+    url = "";
+    return url
+}
---
+++ tvnext/v0/cable.js
@@ -0,0 +1,22 @@
+﻿function getchannels() {
+    let channeldb = [
+        ["index.html", "NULL"],
+        ["index3.html#https://livetv-fa.tubi.video/bbc-earth2/playlist.m3u8", "https://backend-mrdannyarmstrong.github.io/tvnext/v0/ch1.png"],
+        ["index2.html#https://pluto.tv/us/live-tv/5f36d726234ce10007784f2a", "https://backend-mrdannyarmstrong.github.io/tvnext/v0/ch2.png"],
+        ["index3.html#http://fl7.moveonjoy.com/NEWS_NATION/index.m3u8", "https://backend-mrdannyarmstrong.github.io/tvnext/v0/ch3.png"],
+        ["index2.html#http://v2.weatherscan.net/", "https://backend-mrdannyarmstrong.github.io/tvnext/v0/ch4.png"],
+        ["index.html", "NULL"],
+    ];
+    return channeldb;
+}
+
+function getchinfo() {
+    let chinfodb = [
+        ["Channel Name", "Channel Description"],
+        ["Channel Name", "Channel Description"],
+        ["Bob Ross", "24/7 Joy of Painting"],
+        ["News Nation", "Non bias news 24/7"],
+        ["Weather Scan", "24/7 Weather Service"],
+    ];
+    return chinfodb;
+}
