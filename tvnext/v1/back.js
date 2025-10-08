08f6e958f1b02dd52b078f2bcdfe8fab4eabd45e
Daniel <98234578+mrdannyarmstrong@users.noreply.github.com>
2025-10-08 16:28:53 -0400

    Add files via upload

--- tvnext/v1/back.js
+++ tvnext/v1/back.js
@@ -1,1 +1,24 @@
-//add code here
+﻿function getchannels() {
+    let channeldb = [
+        ["index.html", "NULL"],
+        ["index2.html#https://livetv-fa.tubi.video/bbc-earth2/playlist.m3u8", "https://backend-mrdannyarmstrong.github.io/tvnext/v0/ch1.png"],
+        ["https://pluto.tv/us/live-tv/5f36d726234ce10007784f2a", "https://backend-mrdannyarmstrong.github.io/tvnext/v0/ch2.png"],
+        ["index2.html#https://fl7.moveonjoy.com/NEWS_NATION/index.m3u8", "https://backend-mrdannyarmstrong.github.io/tvnext/v0/ch3.png"],
+        ["http://v2.weatherscan.net/", "https://backend-mrdannyarmstrong.github.io/tvnext/v0/ch4.png"],
+        ["index2.html#https://turnerlive.warnermediacdn.com/hls/live/2023168/tnteast/slate/VIDEO_0_3564000.m3u8", "https://backend-mrdannyarmstrong.github.io/tvnext/v0/ch5.png"],
+        ["index2.html#http://api.toonamiaftermath.com:3000/est/playlist.m3u8", "https://backend-mrdannyarmstrong.github.io/tvnext/v0/ch6.png"],
+        ["https://pluto.tv/us/live-tv/65652f7fc0fc88000883537a", "https://backend-mrdannyarmstrong.github.io/tvnext/v0/ch7.png"],
+        ["index2.html#https://media-tyo.hls.nhkworld.jp/hls/w/live/master.m3u8", "https://backend-mrdannyarmstrong.github.io/tvnext/v0/ch8.png"],
+    ];
+    return channeldb;
+}
+
+function checkupdate() {
+    needupdate = 0;
+    return needupdate;
+}
+
+function geturl() {
+    url = "";
+    return url
+}
