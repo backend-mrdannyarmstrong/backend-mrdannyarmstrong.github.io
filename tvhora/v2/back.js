5c6bf40da8e3f0136fc24e00e7536f30521cbaaf
Daniel <98234578+mrdannyarmstrong@users.noreply.github.com>
2025-10-16 17:24:44 -0400

    Add files via upload

--- tvhora/v2/back.js
+++ tvhora/v2/back.js
@@ -1,1 +1,35 @@
-//add code here
+﻿function getchannels() {
+    let channeldb = [
+        ["index.html", "NULL"],
+        ["index.html", "NULL"],
+        ["NULL", "https://backend-mrdannyarmstrong.github.io/tvhora/v2/ch2.png"],
+        ["index2.html#https://fl7.moveonjoy.com/NEWS_NATION/index.m3u8", "https://backend-mrdannyarmstrong.github.io/tvhora/v2/ch3.png"],
+        ["http://v2.weatherscan.net/", "https://backend-mrdannyarmstrong.github.io/tvhora/v2/ch4.png"],
+        ["index2.html#https://fl7.moveonjoy.com/E_ENTERTAINMENT_TELEVISION/index.m3u8", "https://backend-mrdannyarmstrong.github.io/tvhora/v2/ch5.png"],
+        ["index2.html#https://retroblast.tv/hls/stream.m3u8", "https://backend-mrdannyarmstrong.github.io/tvhora/v2/ch6.png"],
+        ["NULL", "https://backend-mrdannyarmstrong.github.io/tvhora/v2/ch7.png"],
+        ["NULL", "https://backend-mrdannyarmstrong.github.io/tvhora/v2/ch8.png"],
+        ["NULL", "https://backend-mrdannyarmstrong.github.io/tvhora/v2/ch9.png"],
+        ["https://www.stvr.sk/embed/live/1", "https://backend-mrdannyarmstrong.github.io/tvhora/v2/ch10.png"],
+        ["https://www.stvr.sk/embed/live/2", "https://backend-mrdannyarmstrong.github.io/tvhora/v2/ch11.png"],
+        ["https://www.stvr.sk/embed/live/3", "https://backend-mrdannyarmstrong.github.io/tvhora/v2/ch12.png"],
+        ["https://www.stvr.sk/embed/live/15", "https://backend-mrdannyarmstrong.github.io/tvhora/v2/ch13.png"],
+        ["https://media.joj.sk/embed/3Yz1gBQeKTs?autoplay=1", "https://backend-mrdannyarmstrong.github.io/tvhora/v2/ch14.png"],
+        ["https://media.joj.sk/embed/DKZwKhUlV3i?autoplay=1", "https://backend-mrdannyarmstrong.github.io/tvhora/v2/ch15.png"],
+        ["https://media.joj.sk/embed/3al7RoqzT3y?autoplay=1", "https://backend-mrdannyarmstrong.github.io/tvhora/v2/ch16.png"],
+        ["https://media.cms.markiza.sk/embed/markiza-live?autoplay=any", "https://backend-mrdannyarmstrong.github.io/tvhora/v2/ch17.png"],
+        ["https://media.cms.markiza.sk/embed/doma-live?autoplay=any", "https://backend-mrdannyarmstrong.github.io/tvhora/v2/ch18.png"],
+        ["https://media.cms.markiza.sk/embed/dajto-live?autoplay=any", "https://backend-mrdannyarmstrong.github.io/tvhora/v2/ch19.png"],
+    ];
+    return channeldb;
+}
+
+function checkupdate() {
+    needupdate = 0;
+    return needupdate;
+}
+
+function geturl() {
+    url = "";
+    return url
+}
