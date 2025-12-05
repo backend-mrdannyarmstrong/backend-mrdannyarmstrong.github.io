e5bd686b3e70959a0a44d5ded1dadd958ae3348a
Daniel <98234578+mrdannyarmstrong@users.noreply.github.com>
2025-12-04 20:16:02 -0400

    Add files via upload

--- tvhora/v2/back.js
+++ tvhora/v2/back.js
@@ -1,1 +1,47 @@
-//add code here
+﻿function getchannels() {
+    let channeldb = [
+        ["index.html", "NULL"],
+        ["index.html", "NULL"],
+        ["https://pluto.tv/us/live-tv/5f36d726234ce10007784f2a", "https://backend-mrdannyarmstrong.github.io/tvhora/v2/ch2.png"],
+        ["index2.html#https://fl1.moveonjoy.com/NEWS_NATION/index.m3u8", "https://backend-mrdannyarmstrong.github.io/tvhora/v2/ch3.png"],
+        ["http://v2.weatherscan.net/", "https://backend-mrdannyarmstrong.github.io/tvhora/v2/ch4.png"],
+        ["index2.html#https://fl1.moveonjoy.com/E_ENTERTAINMENT_TELEVISION/index.m3u8", "https://backend-mrdannyarmstrong.github.io/tvhora/v2/ch5.png"],
+        ["index2.html#https://retroblast.tv/hls/stream.m3u8", "https://backend-mrdannyarmstrong.github.io/tvhora/v2/ch6.png"],
+        ["index2.html#http://api.toonamiaftermath.com:3000/est/playlist.m3u8", "https://backend-mrdannyarmstrong.github.io/tvhora/v2/ch7.png"],
+        ["index2.html#https://turnerlive.warnermediacdn.com/hls/live/2023168/tnteast/slate/VIDEO_0_3564000.m3u8", "https://backend-mrdannyarmstrong.github.io/tvhora/v2/ch8.png"],
+        ["index2.html#https://fl1.moveonjoy.com/AMC_NETWORK/index.m3u8", "https://backend-mrdannyarmstrong.github.io/tvhora/v2/ch9.png"],
+        ["index2.html#https://fl1.moveonjoy.com/FOX_Sports_1/index.m3u8", "https://backend-mrdannyarmstrong.github.io/tvhora/v2/ch10.png"],
+        ["https://www.youtube.com/embed/0T31WVsL9Ro", "https://backend-mrdannyarmstrong.github.io/tvhora/v2/ch11.png"],
+        ["https://www.stvr.sk/embed/live/1", "https://backend-mrdannyarmstrong.github.io/tvhora/v2/ch12.png"],
+        ["https://www.stvr.sk/embed/live/2", "https://backend-mrdannyarmstrong.github.io/tvhora/v2/ch13.png"],
+        ["https://www.stvr.sk/embed/live/3", "https://backend-mrdannyarmstrong.github.io/tvhora/v2/ch14.png"],
+        ["https://www.stvr.sk/embed/live/15", "https://backend-mrdannyarmstrong.github.io/tvhora/v2/ch15.png"],
+        ["https://media.joj.sk/embed/3Yz1gBQeKTs?autoplay=1", "https://backend-mrdannyarmstrong.github.io/tvhora/v2/ch16.png"],
+        ["https://media.joj.sk/embed/DKZwKhUlV3i?autoplay=1", "https://backend-mrdannyarmstrong.github.io/tvhora/v2/ch17.png"],
+        ["https://media.joj.sk/embed/3al7RoqzT3y?autoplay=1", "https://backend-mrdannyarmstrong.github.io/tvhora/v2/ch18.png"],
+        ["index2.html#https://live.cdn.joj.sk/live/andromeda/family-720.m3u8", "https://backend-mrdannyarmstrong.github.io/tvhora/v2/ch19.png"],
+        ["index2.html#https://live.cdn.joj.sk/live/andromeda/cinema-720.m3u8", "https://backend-mrdannyarmstrong.github.io/tvhora/v2/ch20.png"],
+        ["https://media.cms.markiza.sk/embed/markiza-live?autoplay=any", "https://backend-mrdannyarmstrong.github.io/tvhora/v2/ch21.png"],
+        ["https://media.cms.markiza.sk/embed/doma-live?autoplay=any", "https://backend-mrdannyarmstrong.github.io/tvhora/v2/ch22.png"],
+        ["https://media.cms.markiza.sk/embed/dajto-live?autoplay=any", "https://backend-mrdannyarmstrong.github.io/tvhora/v2/ch23.png"],
+        ["index2.html#https://us1001.utako.moe/NHK_G/index.m3u8", "https://backend-mrdannyarmstrong.github.io/tvhora/v2/ch24.png"],
+        ["index2.html#https://us1001.utako.moe/NHK_E/index.m3u8", "https://backend-mrdannyarmstrong.github.io/tvhora/v2/ch25.png"],
+        ["index2.html#https://us1001.utako.moe/Nippon_TV/index.m3u8", "https://backend-mrdannyarmstrong.github.io/tvhora/v2/ch26.png"],
+        ["index2.html#https://us1001.utako.moe/TV_Asahi/index.m3u8", "https://backend-mrdannyarmstrong.github.io/tvhora/v2/ch27.png"],
+        ["index2.html#https://us1001.utako.moe/TBS/index.m3u8", "https://backend-mrdannyarmstrong.github.io/tvhora/v2/ch28.png"],
+        ["index2.html#https://us1001.utako.moe/TV_Tokyo/index.m3u8", "https://backend-mrdannyarmstrong.github.io/tvhora/v2/ch29.png"],
+        ["index2.html#https://us1001.utako.moe/Fuji_TV/index.m3u8", "https://backend-mrdannyarmstrong.github.io/tvhora/v2/ch30.png"],
+        ["index2.html#https://us1001.utako.moe/Tokyo_MX1/index.m3u8", "https://backend-mrdannyarmstrong.github.io/tvhora/v2/ch31.png"],
+    ];
+    return channeldb;
+}
+
+function checkupdate() {
+    needupdate = 0;
+    return needupdate;
+}
+
+function geturl() {
+    url = "";
+    return url
+}
