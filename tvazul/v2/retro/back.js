fb1e17930b7c14a8fe2af7ac111b14464dfad256
Daniel <98234578+mrdannyarmstrong@users.noreply.github.com>
2025-12-15 16:53:47 -0400

    Add files via upload

--- tvazul/v2/retro/back.js
+++ tvazul/v2/retro/back.js
@@ -1,1 +1,30 @@
-//add code here
+﻿function getchannels() {
+    let channeldb = [
+        ["index.html", "NULL"],
+        ["index.html", "NULL"],
+        ["index2.html#https://amg02333-pbs-amg02333c6-firetv-us-4236.playouts.now.amagi.tv/playlist.m3u8", "https://backend-mrdannyarmstrong.github.io/tvazul/v2/retro/ch2.png"],
+        ["index2.html#https://fl1.moveonjoy.com/NEWS_NATION/index.m3u8", "https://backend-mrdannyarmstrong.github.io/tvazul/v2/retro/ch3.png"],
+        ["http://v1.weatherscan.net/", "https://backend-mrdannyarmstrong.github.io/tvazul/v2/retro/ch4.png"],
+        ["index2.html#https://fl1.moveonjoy.com/COZI_TV/index.m3u8", "https://backend-mrdannyarmstrong.github.io/tvazul/v2/retro/ch5.png"],
+        ["index2.html#https://retroblast.tv/hls/stream.m3u8", "https://backend-mrdannyarmstrong.github.io/tvazul/v2/retro/ch6.png"],
+        ["index2.html#http://api.toonamiaftermath.com:3000/est/playlist.m3u8", "https://backend-mrdannyarmstrong.github.io/tvazul/v2/retro/ch7.png"],
+        ["https://pluto.tv/us/live-tv/62ba60f059624e000781c436", "https://backend-mrdannyarmstrong.github.io/tvazul/v2/retro/ch8.png"],
+        ["index2.html#https://fl1.moveonjoy.com/MTV_CLASSIC/index.m3u8", "https://backend-mrdannyarmstrong.github.io/tvazul/v2/retro/ch9.png"],
+        ["index2.html#https://streaming-live.rtp.pt/liverepeater/rtpmem.smil/playlist.m3u8", "https://backend-mrdannyarmstrong.github.io/tvazul/v2/retro/ch10.png"],
+        ["index2.html#https://streaming-live.rtp.pt/liverepeater/smil:rtpnHD.smil/playlist.m3u8", "https://backend-mrdannyarmstrong.github.io/tvazul/v2/retro/ch11.png"],
+        ["index2.html#https://production-fast-sic.content.okast.tv/fa2e8c4385712f9a7dce4ff2dcebac2e/channels/d9070446-8448-455e-8075-773b1ba12562/d47eae0f-ad77-414a-9a1d-2a6628ba18c3/media_.m3u8", "https://backend-mrdannyarmstrong.github.io/tvazul/v2/retro/ch12.png"],
+        ["index2.html#https://video-auth2.iol.pt/live_tvi_ficcao/live_tvi_ficcao/edge_servers/tvificcao-720p/playlist.m3u8", "https://backend-mrdannyarmstrong.github.io/tvazul/v2/retro/ch13.png"],
+        
+    ];
+    return channeldb;
+}
+
+function checkupdate() {
+    needupdate = 0;
+    return needupdate;
+}
+
+function geturl() {
+    url = "";
+    return url
+}
---
+++ tvazul/v2/retro/cable.js
@@ -0,0 +1,43 @@
+﻿function getchannels() {
+    let channeldb = [
+        ["index.html", "NULL"],
+        ["index.html", "NULL"],
+        ["index.html", "NULL"],
+        ["index.html", "NULL"],
+        ["index3.html#https://amg02333-pbs-amg02333c6-firetv-us-4236.playouts.now.amagi.tv/playlist.m3u8", "https://backend-mrdannyarmstrong.github.io/tvazul/v2/retro/ch2.png"],
+        ["index3.html#https://fl1.moveonjoy.com/NEWS_NATION/index.m3u8", "https://backend-mrdannyarmstrong.github.io/tvazul/v2/retro/ch3.png"],
+        ["index2.html#http://v1.weatherscan.net/", "https://backend-mrdannyarmstrong.github.io/tvazul/v2/retro/ch4.png"],
+        ["index3.html#https://fl1.moveonjoy.com/COZI_TV/index.m3u8", "https://backend-mrdannyarmstrong.github.io/tvazul/v2/retro/ch5.png"],
+        ["index3.html#https://retroblast.tv/hls/stream.m3u8", "https://backend-mrdannyarmstrong.github.io/tvazul/v2/retro/ch6.png"],
+        ["index3.html#http://api.toonamiaftermath.com:3000/est/playlist.m3u8", "https://backend-mrdannyarmstrong.github.io/tvazul/v2/retro/ch7.png"],
+        ["index2.html#https://pluto.tv/us/live-tv/62ba60f059624e000781c436", "https://backend-mrdannyarmstrong.github.io/tvazul/v2/retro/ch8.png"],
+        ["index3.html#https://fl1.moveonjoy.com/MTV_CLASSIC/index.m3u8", "https://backend-mrdannyarmstrong.github.io/tvazul/v2/retro/ch9.png"],
+        ["index3.html#https://streaming-live.rtp.pt/liverepeater/rtpmem.smil/playlist.m3u8", "https://backend-mrdannyarmstrong.github.io/tvazul/v2/retro/ch10.png"],
+        ["index3.html#https://streaming-live.rtp.pt/liverepeater/smil:rtpnHD.smil/playlist.m3u8", "https://backend-mrdannyarmstrong.github.io/tvazul/v2/retro/ch11.png"],
+        ["index3.html#https://production-fast-sic.content.okast.tv/fa2e8c4385712f9a7dce4ff2dcebac2e/channels/d9070446-8448-455e-8075-773b1ba12562/d47eae0f-ad77-414a-9a1d-2a6628ba18c3/media_.m3u8", "https://backend-mrdannyarmstrong.github.io/tvazul/v2/retro/ch12.png"],
+        ["index3.html#https://video-auth2.iol.pt/live_tvi_ficcao/live_tvi_ficcao/edge_servers/tvificcao-720p/playlist.m3u8", "https://backend-mrdannyarmstrong.github.io/tvazul/v2/retro/ch13.png"],
+        ["index.html", "NULL"],
+    ];
+    return channeldb;
+} 
+
+function getchinfo() {
+    let chinfodb = [
+        ["Channel Name", "Channel Description"],
+        ["Channel Name", "Channel Description"],
+        ["PBS", "General contenr"],
+        ["News Nation", "Non bias news 24/7"],
+        ["The Weather Channel", "Weather info 24/7"],
+        ["Cozy TV", "The most popular TV shows"],
+        ["Cartoon Network", "Cartoons"],
+        ["Toonami Aftermath", "Cartoons and Anime"],
+        ["00s Replay", "Movies from the 2000s"],
+        ["MTV", "Music Channel"],
+        ["RTP 1", "Conteúdo geral"],
+        ["RTP 2", "Conteúdo variado"],
+        ["SIC", "Telenovelas"],
+        ["TVI", "Telenovalas e variado"],
+        ["Channel Name", "Channel Description"],
+    ];
+    return chinfodb;
+}
