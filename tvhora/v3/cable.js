4af5502f90b85c32137840b6a367e3fa5060b46d
Daniel <98234578+mrdannyarmstrong@users.noreply.github.com>
2026-02-21 12:03:50 -0400

    Add files via upload

--- tvhora/v3/cable.js
+++ tvhora/v3/cable.js
@@ -31,7 +31,7 @@
         ["index2.html#https://pluto.tv/us/live-tv/5f36d726234ce10007784f2a", "https://backend-mrdannyarmstrong.github.io/tvhora/v3/ch28.png"],
         ["index3.html#https://content.uplynk.com/channel/1f93c13275024afb9e0ead299624073d.m3u8", "https://backend-mrdannyarmstrong.github.io/tvhora/v3/ch29.png"],
         ["index2.html#https://www.wwlp.com/watch-live/", "https://backend-mrdannyarmstrong.github.io/tvhora/v3/ch30.png"],
-        ["index2.htmlhttp://v2.weatherscan.net/", "https://backend-mrdannyarmstrong.github.io/tvhora/v3/ch31.png"],
+        ["index2.html#http://v2.weatherscan.net/", "https://backend-mrdannyarmstrong.github.io/tvhora/v3/ch31.png"],
         ["index3.html#https://retroblast.tv/hls/stream.m3u8", "https://backend-mrdannyarmstrong.github.io/tvhora/v3/ch32.png"],
         ["index3.html#https://turnerlive.warnermediacdn.com/hls/live/2023183/aseast/noslate/VIDEO_1_5128000.m3u8", "https://backend-mrdannyarmstrong.github.io/tvhora/v3/ch33.png"],
         ["index3.html#https://turnerlive.warnermediacdn.com/hls/live/2023168/tnteast/slate/VIDEO_0_3564000.m3u8", "https://backend-mrdannyarmstrong.github.io/tvhora/v3/ch34.png"],
