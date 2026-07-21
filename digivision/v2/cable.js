0d9069d0fb1a875c9fb23079a4bd2c494f6fad6f
Daniel <98234578+mrdannyarmstrong@users.noreply.github.com>
2026-07-21 15:01:51 -0400

    Add files via upload

--- digivision/v2/cable.js
+++ digivision/v2/cable.js
@@ -1,52 +1,4 @@
-﻿function getchannels() {
-    let channeldb = [
-        ["index4.html", "NULL"],
-        ["index3.html#https://hls6eu.sweet.tv/stream/9vc5sh3ws32idimixvu47ryuctcs6tjsjegyzr9z3bamjg46n5ihekmbrpw4shunikktiagg29iv35xrra6x2nrcb8h97szk9q3sdim4f9wm4itev8nq3bt4ip4q7a8ysrfeada694jv3efbmgsq2pq8y86f2wuv98h23tyfjt5e2t28wgnqbyzzg4j7ymxmntnwqu3a.m3u8", "https://backend-mrdannyarmstrong.github.io/digivision/v2/ch1.png"],
-        ["index3.html#https://hls3eu.sweet.tv/stream/pv288u4tiszs28eusv7rkx5g242terfn3msvpxd89c8ajx85qwbj6kfnzutikxbe2rj6y9cam43a32zbkdy2v3js2chczszwmjrg2cd93b474sik8v45fnuadj4cy8g38n8zba7fahxwa6zhrdkkd7buka3dx3ueyfqgwjb22exkrh3i73w888p7afxpi5d3pc3wdwi=.m3u8", "https://backend-mrdannyarmstrong.github.io/digivision/v2/ch2.png"],
-        ["index2.html#https://www.tvp.info/ogladaj-na-zywo", "https://backend-mrdannyarmstrong.github.io/digivision/v2/ch3.png"],
-        ["index3.html#https://hls6eu.sweet.tv/stream/8e4vua775pfspiknhfkn3jmyhp665w9ir79yce4fs5fnavef97k3dtxz4a3pr66u6gvrvwjtuh6wfbrry56y2md4erufdu34wyk2t6g4j6kc6gbz65eqaa4msfcmpsdffjzpvktdpxgyv6nenzixbf9uvcunbhcdcccsip28m2wf27skj9vugvq3f23hrq7imm3xfra=.m3u8", "https://backend-mrdannyarmstrong.github.io/digivision/v2/ch4.png"],
-        ["index2.html#https://pilot.wp.pl/tv/#polsat-hd", "https://backend-mrdannyarmstrong.github.io/digivision/v2/ch5.png"],
-        ["index2.html#https://tvivu.com/watch/polsat-film", "https://backend-mrdannyarmstrong.github.io/digivision/v2/ch6.png"],
-        ["index3.html#https://cdn-s-lb2.pluscdn.pl/lv/1517830/349/hls/f03a76f3/masterlist.m3u8", "https://backend-mrdannyarmstrong.github.io/digivision/v2/ch7.png"],
-        ["index2.html#https://pilot.wp.pl/tv/#tv-puls-hd", "https://backend-mrdannyarmstrong.github.io/digivision/v2/ch8.png"],
-        ["index2.html#https://pilot.wp.pl/tv/#tv-4-hd", "https://backend-mrdannyarmstrong.github.io/digivision/v2/ch9.png"],
-        ["index3.html#https://stream.4fun.tv:8888/hls/4f_high/index.m3u8", "https://backend-mrdannyarmstrong.github.io/digivision/v2/ch10.png"],
-        ["index2.html#https://pluto.tv/us/live-tv/5f36d726234ce10007784f2a", "https://backend-mrdannyarmstrong.github.io/digivision/v2/ch11.png"],
-        ["index2.html#https://www.newsnationnow.com/news-nation-live/", "https://backend-mrdannyarmstrong.github.io/digivision/v2/ch12.png"],
-        ["index2.html#http://v1.weatherscan.net/", "https://backend-mrdannyarmstrong.github.io/digivision/v2/ch13.png"],
-        ["index3.html#https://retroblast.tv/hls/stream.m3u8", "https://backend-mrdannyarmstrong.github.io/digivision/v2/ch14.png"],
-        ["index3.html#https://asp7.toonamiaftermath.com/livehttporigin/est/Ibd7c5-xHCbpk-playlist.m3u8", "https://backend-mrdannyarmstrong.github.io/digivision/v2/ch15.png"],
-        ["index3.html#https://turnerlive.warnermediacdn.com/hls/live/2023168/tnteast/slate/VIDEO_0_3564000.m3u8", "https://backend-mrdannyarmstrong.github.io/digivision/v2/ch16.png"],
-        ["index.html", "NULL"],
-    ];
-    return channeldb;
-}
-
-function getchinfo() {
-    let chinfodb = [
-        ["Channel Name", "Channel Description"],
-        ["TVP 1", "Všeobecný obsah"],
-        ["TVP 2", "Różnorodna treść"],
-        ["TVP Info", "Wiadomości 24/7"],
-        ["TVP Sports", "Kanał sportowy"],
-        ["Polsat", "Seriale i filmy"],
-        ["Polsat Film", "kanał filmowy"],
-        ["Polsat News", "Wiadomości 24/7"],
-        ["TV Puls", "Różnorodna treść"],
-        ["TV4", "Seriale i filmy"],
-        ["4Fun TV", "Kanał muzyczny"],
-        ["Bob Ross", "Joy of Painting"],
-        ["News Nation", "Non bias news 24/7"],
-        ["Weather Scan", "Weather info 24/7"],
-        ["Cartoon Network", "Cartoons"],
-        ["Toonami Aftermath", "Cartoons and Anime"],
-        ["TNT", "Movies TV shows and sports"],
-        ["Channel Name", "Channel Description"],
-    ];
-    return chinfodb;
-}
-
-function getwebchannels() {
+﻿function getwebchannels() {
     let channeldb = [
         ["index2.html#https://hls6eu.sweet.tv/stream/9vc5sh3ws32idimixvu47ryuctcs6tjsjegyzr9z3bamjg46n5ihekmbrpw4shunikktiagg29iv35xrra6x2nrcb8h97szk9q3sdim4f9wm4itev8nq3bt4ip4q7a8ysrfeada694jv3efbmgsq2pq8y86f2wuv98h23tyfjt5e2t28wgnqbyzzg4j7ymxmntnwqu3a.m3u8", "https://backend-mrdannyarmstrong.github.io/digivision/v2/ch1.png"],
         ["index2.html#https://hls3eu.sweet.tv/stream/pv288u4tiszs28eusv7rkx5g242terfn3msvpxd89c8ajx85qwbj6kfnzutikxbe2rj6y9cam43a32zbkdy2v3js2chczszwmjrg2cd93b474sik8v45fnuadj4cy8g38n8zba7fahxwa6zhrdkkd7buka3dx3ueyfqgwjb22exkrh3i73w888p7afxpi5d3pc3wdwi=.m3u8", "https://backend-mrdannyarmstrong.github.io/digivision/v2/ch2.png"],
@@ -57,13 +9,15 @@ function getwebchannels() {
         ["index2.html#https://cdn-s-lb2.pluscdn.pl/lv/1517830/349/hls/f03a76f3/masterlist.m3u8", "https://backend-mrdannyarmstrong.github.io/digivision/v2/ch7.png"],
         ["https://player.pl/live/tv-puls,9353728", "https://backend-mrdannyarmstrong.github.io/digivision/v2/ch8.png"],
         ["https://player.pl/live/tv-4,11046757", "https://backend-mrdannyarmstrong.github.io/digivision/v2/ch9.png"],
-        ["index2.html#https://stream.4fun.tv:8888/hls/4f_high/index.m3u8", "https://backend-mrdannyarmstrong.github.io/digivision/v2/ch10.png"],
-        ["https://pluto.tv/us/live-tv/5f36d726234ce10007784f2a", "https://backend-mrdannyarmstrong.github.io/digivision/v2/ch11.png"],
-        ["https://www.newsnationnow.com/news-nation-live/", "https://backend-mrdannyarmstrong.github.io/digivision/v2/ch12.png"],
-        ["http://v1.weatherscan.net/", "https://backend-mrdannyarmstrong.github.io/digivision/v2/ch13.png"],
-        ["index2.html#https://retroblast.tv/hls/stream.m3u8", "https://backend-mrdannyarmstrong.github.io/digivision/v2/ch14.png"],
-        ["index2.html#https://asp7.toonamiaftermath.com/livehttporigin/est/Ibd7c5-xHCbpk-playlist.m3u8", "https://backend-mrdannyarmstrong.github.io/digivision/v2/ch15.png"],
+        ["https://player.pl/live/tvn-hd,1908473", "https://backend-mrdannyarmstrong.github.io/digivision/v2/ch10.png"],
+        ["index2.html#https://stream.4fun.tv:8888/hls/4f_high/index.m3u8", "https://backend-mrdannyarmstrong.github.io/digivision/v2/ch11.png"],
+        ["index2.html#https://aegis-cloudfront-1.tubi.video/bb1fc6ad-9948-42ea-aaf3-20acfcdeecac/playlist.m3u8", "https://backend-mrdannyarmstrong.github.io/digivision/v2/ch12.png"],
+        ["https://www.newsnationnow.com/news-nation-live/", "https://backend-mrdannyarmstrong.github.io/digivision/v2/ch13.png"],
+        ["http://v1.weatherscan.net/", "https://backend-mrdannyarmstrong.github.io/digivision/v2/ch16.png"],
+        ["index2.html#https://retroblast.tv/hls/stream.m3u8", "https://backend-mrdannyarmstrong.github.io/digivision/v2/ch15.png"],
         ["index2.html#https://turnerlive.warnermediacdn.com/hls/live/2023168/tnteast/slate/VIDEO_0_3564000.m3u8", "https://backend-mrdannyarmstrong.github.io/digivision/v2/ch16.png"],
+        ["https://tvnow247.top/watch/e-entertainment-television/", "https://backend-mrdannyarmstrong.github.io/digivision/v2/ch17.png"],
+        ["https://backend-mrdannyarmstrong.github.io/digivision/v2/yt/music.html", "https://backend-mrdannyarmstrong.github.io/digivision/v2/ch18.png"],
     ];
     return channeldb;
 }
